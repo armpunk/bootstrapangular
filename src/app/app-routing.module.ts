a03eb151327eb4bb5e2b70de9c1e805c1765e029
arm <untaz2003@gmail.com>
2020-12-22 15:03:29 +0800

    add bootstrapangular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { 
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+    data: { title: 'Heroes List' }
+  },
+  { 
+    path: 'home', 
+    component: HomeComponent ,
+    data: { title: 'Heroes List' }
+  },
+  { 
+    path: 'table', 
+    component: TableComponent,
+    data: { title: 'Table' }
+  },
+  { 
+    path: '**', 
+    component: PageNotFoundComponent 
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
