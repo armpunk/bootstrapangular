e732b79a9393578112c7ebfbc4db848b40bc51ea
arm <untaz2003@gmail.com>
2020-12-22 18:32:34 +0800

    push all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'; 
+
+import { DataTablesModule } from 'angular-datatables';
+
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +20,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    HttpClientModule
   ],
   providers: [Title], 
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
+class Student {
+  id: number;
+  name: string;
+  emel: string;
+  website: string;
+}
+
+class DataTablesResponse {
+  data: any[];
+  draw: number;
+  recordsFiltered: number;
+  recordsTotal: number;
+}
 
 @Component({
   selector: 'app-table',
@@ -7,9 +22,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  dtOptions: any = {};
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    this.dtOptions = {
+      ajax: 'http://localhost/startbootstrapapi/index.php/api/get',
+      columns: [{
+        title: 'ID',
+        data: 'id'
+      }, 
+      {
+        title: 'Name',
+        data: 'name'
+      }, 
+      {
+        title: 'Emel',
+        data: 'emel'
+      }, 
+      {
+        title: 'Website',
+        data: 'website'
+      }],
+      dom: 'Bfrtip',
+      buttons: [
+        'columnsToggle',
+        'colvis',
+        'copy',
+        'print',
+        'excel'
+      ]
+    }
   }
+
+  // ngOnInit(): void {
+  //   const that = this;
+  //   this.dtOptions = {
+  //     destroy: true,
+  //     pagingType: 'full_numbers',
+  //     pageLength: 2,
+  //     serverSide: true,
+  //     // processing: true,
+  //     ajax: (dataTablesParameters: any, callback) => {
+  //       that.http
+  //         .post<DataTablesResponse>(
+  //           'http://localhost/startbootstrapapi/index.php/api/get',
+  //           dataTablesParameters, {}
+  //         ).subscribe(resp => {
+  //           that.students = resp.data;
+
+  //           callback({
+  //             recordsTotal: resp.recordsTotal,
+  //             recordsFiltered: resp.recordsFiltered,
+  //             data: []
+  //           });
+  //         });
+  //     },
+  //     columns: [{ data: 'id' }, { data: 'name' }, { data: 'emel' }, { data: 'website' }],
+  //     dom: 'Bfrtip',
+  //     buttons: [
+  //       'copy',
+  //       'print',
+  //       'excel',
+  //       'csv'
+  //     ]
+  //   };
+  // }
 
 }
